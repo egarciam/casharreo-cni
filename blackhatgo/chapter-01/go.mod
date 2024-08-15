acba82e991cfa8832287e51d8501147103a7d6b6
Ernesto Garcia del moral G <ernesto.garcia@orange.com>
2024-08-15 11:09:21 +0200

    first ps example in go

---
+++ blackhatgo/chapter-01/go.mod
@@ -0,0 +1,3 @@
+module egarciam.com/ps
+
+go 1.22.4
---
+++ blackhatgo/chapter-01/ps-1.go
@@ -0,0 +1,14 @@
+package main
+
+import "net"
+
+func main() {
+	_, err := net.Dial("tcp", "scanme.nmap.org:80")
+	if err == nil {
+		println("Connection successful")
+	} else {
+		println("Failed")
+	}
+
+	return
+}
